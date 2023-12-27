92477d38f810c028cfd68423ff8a80aab5e271b5
Cory Daddona <corydaddona1@hotmail.com>
2023-12-27 15:43:54 -0600

    updated project files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# coe924bbe83c03b598d70a105
+# co6384f628fb1a3d8294263fd
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
---
+++ api.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRZfkb-cDgfB2uRz2RY28Z2SCy3YnERDM",
+  authDomain: "vanlife-14b9b.firebaseapp.com",
+  projectId: "vanlife-14b9b",
+  storageBucket: "vanlife-14b9b.appspot.com",
+  messagingSenderId: "152464252936",
+  appId: "1:152464252936:web:1b9d865f392737a0c131bd"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
